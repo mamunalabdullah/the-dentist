7d0c57ff58f589357e79525b3a46d199f6a9ea25
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-04-17 01:11:41 +0600

    header done

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <h1>header</h1>
+            <>
+            <Navbar className='navbar py-4 bg-info'>
+                <Container>
+                    <Link className='logo' to="/home">Find The Dantist</Link>
+                    <Nav>
+                        <NavLink className={({isActive}) =>
+                        isActive ? "active-link" : "link"} to="/home">Home</NavLink>
+                        <NavLink className={({isActive}) =>
+                        isActive ? "active-link" : "link"} to="/about">About</NavLink>
+                        <NavLink className={({isActive}) =>
+                        isActive ? "active-link" : "link"} to="/blog">Blog</NavLink>
+                        <NavLink className={({isActive}) =>
+                        isActive ? "active-link" : "link"} to="/login">Login</NavLink>
+                        <NavLink className={({isActive}) =>
+                        isActive ? "active-link" : "link"} to="/register">Sign up</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+            </>
         </div>
     );
 };
