e9c5734096f89a3a8b820d31f435f1d70794bc14
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-04-17 03:09:11 +0600

    banner add

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,10 +1,16 @@
+import { faTooth } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>banner</h1>
+        <div className='banner'>
+            <div className="banner-title p-2">
+                <p className='p-2'>MARY DOE <span className='text-danger'>Dantist <FontAwesomeIcon icon={faTooth} /></span>
+                </p>
+                <h1 className='w-50 p-2'>Hi! my name is <span className='colorful'>Mary Doe</span> I am a <span className='colorful'>Doctor</span>.</h1>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../../Banner/Banner';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <Banner />
         </div>
     );
 };
