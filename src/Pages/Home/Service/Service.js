1584637a6e8f2864c49e5894bb53eebddbdf24cf
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-04-17 19:42:06 +0600

    service add

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Service.css'
 
-const Service = () => {
+const Service = ({service}) => {
+    const { picture, price, name, description} = service;
     return (
         <div>
-            
+            <Card style={{ width: '18rem' }} className='service-card'>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    {description.slice(0, 200)+'...........'}
+                </Card.Text>
+                <h1>$ {price}</h1>
+                </Card.Body>
+                <Link to="/checkout" className='button-link py-2'>Checkout</Link>
+            </Card>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
+    const [services, setServices] =useState([]);
+
+    useEffect( () => {
+        fetch("services.json")
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, []);
+
     return (
-        <div>
-            
+        <div className='service'>
+            <div className='container text-center py-2'>
+            <h1 className='text-danger py-5'>My Services</h1>
+            <div className='services'>
+                {
+                    services.map( service => <Service 
+                        key={service.id}
+                        service={service}
+                    />)
+                }
+            </div>
+        </div>
         </div>
     );
 };
